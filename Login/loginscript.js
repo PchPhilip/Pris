75c89bf02b7a4b035f4bd2b8f0fd99c8e50b61c0
Pchphilip <chroona.philip@elev.ga.ntig.se>
2021-11-14 00:41:04 +0100

    LoginPage

---
+++ Login/loginscript.js
@@ -0,0 +1,30 @@
+var DBUsers = [
+  {
+    username: "test",
+    password: "123",
+  },
+  {
+    username: "test2",
+    password: "234",
+  },
+  {
+    username: "test3",
+    password: "345",
+  },
+];
+
+function validateUser() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  for (i = 0; i < DBUsers.length; i++) {
+    if (username == DBUsers[i].username && password == DBUsers[i].password) {
+      document.cookie = "username=" + username;
+      document.cookie = "password=" + password;
+      console.log("Inloggad som" + username);
+
+      return;
+    }
+    console.log("Fel lösenord eller användarnamn!");
+  }
+}
