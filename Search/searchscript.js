1164b3cbf607b977335fea4f6ba950f8a1f292f4
Pchphilip <chroona.philip@elev.ga.ntig.se>
2021-11-19 21:39:41 +0100

    1.05JS

--- Search/searchscript.js
+++ Search/searchscript.js
@@ -1,5 +1,24 @@
+// Kör koden när sidan laddas in.
 window.addEventListener("load", () => {
-  const search = sessionStorage.getItem("textsearch");
-  var search2 = document.getElementById("textsearch");
-  document.getElementById("search-result").innerHTML = search2;
+  //Hämtar sökvärdet som är lagrat tillfälligt i sessionen.
+  const search = sessionStorage.getItem("textse");
+  document.getElementById("search-result").innerHTML = search;
+  document.getElementsByName("text")[0].placeholder = search;
+
+  // Slumpar sökningens resultat då jag inte hann slutföra mitt API för att hämta riktigta produkter.
+  var ulist = document.querySelectorAll("ul");
+  var list = ulist[0].querySelectorAll("li").length;
+  for (var i = 0; i < list; i++) {
+    var randomselect = Math.floor(Math.random() * list);
+    ulist.forEach(function (random) {
+      random.appendChild(random.querySelectorAll("li")[randomselect]);
+    });
+  }
 });
+
+// Om man söker igen.
+function sendsearch() {
+  const search = document.getElementById("textsearch").value;
+  sessionStorage.setItem("textse", search);
+  return;
+}
--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// Slumpar Deals!
+document.addEventListener("DOMContentLoaded", function () {
+  var ulist = document.querySelectorAll("ul");
+  var list = ulist[0].querySelectorAll("li").length;
+  for (var i = 0; i < list; i++) {
+    var randomselect = Math.floor(Math.random() * list);
+    ulist.forEach(function (random) {
+      random.appendChild(random.querySelectorAll("li")[randomselect]);
+    });
+  }
+});
+
+function randomdeals() {}
+
+// Om man söker lagras värdet tillfälligt i sessionen som sedan hämtas av "searchscript.js"
 function sendsearch() {
-  const search = document.getElementById("text").ariaValueMax;
-  sessionStorage.setItem("text", search);
+  const search = document.getElementById("textsearch").value;
+  sessionStorage.setItem("textse", search);
   return;
 }
