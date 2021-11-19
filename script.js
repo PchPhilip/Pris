d2e191d7eaeaa3db5b1353ed92bc7a784b117532
Pchphilip <chroona.philip@elev.ga.ntig.se>
2021-11-19 22:05:56 +0100

    ScriptUpdate

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Slumpar Deals!
-document.addEventListener("DOMContentLoaded", function () {
-  var ulist = document.querySelectorAll("ul");
-  var list = ulist[0].querySelectorAll("li").length;
-  for (var i = 0; i < list; i++) {
-    var randomselect = Math.floor(Math.random() * list);
-    ulist.forEach(function (random) {
-      random.appendChild(random.querySelectorAll("li")[randomselect]);
-    });
-  }
-});
-
-function randomdeals() {}
-
 // Om man söker lagras värdet tillfälligt i sessionen som sedan hämtas av "searchscript.js"
 function sendsearch() {
   const search = document.getElementById("textsearch").value;
